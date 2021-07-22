3cf4ba2ed3987d7f5c06182caa9a6eae9c7a9055
Tamara Stamenic <tamara.stamenic@united.cloud>
2021-07-22 11:14:24 +0200

    second commit

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,8 +1,10 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    List<Post> findByCategory(Category category);
 }
